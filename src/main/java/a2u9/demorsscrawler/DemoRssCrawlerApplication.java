14dae03b68628e96bce7fd4029932b3c58772b8c
a2u9 <a2u9dev@gmail.com>
2018-07-28 22:20:04 +0500

    DemoRssCrawler :: Spring Boot, Maven + Web, REST, Security, Quartz, JPA, H2

---
+++ src/main/java/a2u9/demorsscrawler/DemoRssCrawlerApplication.java
@@ -0,0 +1,12 @@
+package a2u9.demorsscrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRssCrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRssCrawlerApplication.class, args);
+	}
+}
